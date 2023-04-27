a7ba0cb0c4b9d9cac9f60a1472b873e820ebe15b
Hira Siddiqui <hisi@mms-t-systems.de>
2023-04-27 19:13:28 +0200

    Commitment passed from extension to react web app

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,10 +1,18 @@
-import logo from '../images/logo.png';
-
 window.onload = () => {
-    document.dispatchEvent(new CustomEvent('ContentScriptEvent', {detail: "Hello from Content Script"}));
-    alert("Hello Event Dispatched");
+    //document.dispatchEvent(new CustomEvent('ContentScriptEvent', {detail: "Hello from Content Script"}));
 }
-export const sendIdentityCommitment = (identityCommitment: string) => {
-    document.dispatchEvent(new CustomEvent('IdentityEvent', {detail: identityCommitment}));
-    alert ("Identity commitment sent: "+identityCommitment);   
+
+export const sendIdentityCommitment = (identityCommitment: string) => {    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id!!, {action: "receive_active_identity", commitment: identityCommitment}, function(response) {});  
+    });
 }
+
+chrome.runtime.onMessage.addListener(function(msg: {action: string, commitment: string}, sender, sendResponse) {
+
+    if (msg.action == 'receive_active_identity') {
+      alert("Message recieved!: "+msg.commitment);
+      localStorage.setItem("commitment",msg.commitment);
+    }
+  });
+
